e038ff63950810b7fbf25d434ea7d492d4d37202
firdaamalia <42663503+firdaamalia@users.noreply.github.com>
2018-09-13 09:03:46 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,29 @@
-<?php
+<?php 
+	$brs= 8;
+    printPyramidaa(0);
+	 function printPyramidaa($f){
+	  global $brs;
+	  printSpace(0,$f);
+	  printStar($brs,$f);
+	  echo "<br>";
+	if (++$f <$brs);
+	  printPyramidaa($f);
+    }
+	 function printSpace($i,$f){
+		global $brs;
+	if ($i > $brs - $f - 1){
+	echo "&nbsp;";
+	printSpace($i + 1,$f);
+	}
+	}
+	 function printStar($r,$f){
+		global $brs;
+		if ($r > $brs - $f -1){
+		echo " *&nbsp; " ;
+		printStar($r - 1,$f);
+		}
+	}
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
-}
-rekursif(5);
-?>
+
+
+ ?>
